4e729f33296a77ad7f15a0ebaec06309748049b1
Marcus Noble <marcus@marcusnoble.co.uk>
2015-04-16 06:21:00 +0100

    JSOxbot will now respond to anyone asking, not just when asked directly

--- scripts/meetup.js
+++ scripts/meetup.js
@@ -11,7 +11,7 @@
 //   hubot when is the next event ?  - returns the next upcoming meetup event
 
 module.exports = function(robot) {
-    robot.respond(/when(s|'s| is) the next (meetup|event|jsoxford)\s?\?/i, function(msg){
+    robot.hear(/when(s|'s| is) the next (meetup|event|jsoxford)\s?\?/i, function(msg){
         var upcomingEventsQuery = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_id=17778422&only=time%2Cevent_url%2Cname%2Cdescription%2Cyes_rsvp_count%2Crsvp_limit&photo-host=secure&page=20&fields=&order=time&desc=false&status=upcoming&sig_id=153356042&sig=84e9ac6ce37bdb3c00e4f82fe5a7ce798865fbe4";
         robot.http(upcomingEventsQuery).get()(function(err, res, body){
           if(err) console.log(err);
