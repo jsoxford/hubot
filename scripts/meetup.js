6fa4a26e8da14e1b59500572853fe910b64e5dfb
Marcus Noble <AverageMarcus@users.noreply.github.com>
2015-04-20 13:38:21 +0100

    Updated memebers URL

--- scripts/meetup.js
+++ scripts/meetup.js
@@ -12,7 +12,7 @@
 //   how many members do we have? - returns the number of meetup members
 
 module.exports = function(robot) {
-  var membersQuery = "https://api.meetup.com/2/members?offset=0&format=json&group_id=17778422&only=photo%2Cname%2Clink&photo-host=secure&page=200&order=name&sig_id=153356042&sig=4d8e3265b4374b84aabb8efcc26eb8107a3ec81b";
+  var membersQuery = "https://api.meetup.com/2/members?offset=0&format=json&group_id=17778422&only=photo%2Cname%2Clink&photo-host=public&page=400&order=name&sig_id=153356042&sig=882696feeed26ad167774539a3da2295dd1fe96b";
   var upcomingEventsQuery = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_id=17778422&only=time%2Cevent_url%2Cname%2Cdescription%2Cyes_rsvp_count%2Crsvp_limit&photo-host=secure&page=20&fields=&order=time&desc=false&status=upcoming&sig_id=153356042&sig=84e9ac6ce37bdb3c00e4f82fe5a7ce798865fbe4";
   var meetupCheckInterval, result;
   var room = "jsoxford/jsoxford.github.com";
