6e06c8df5521d5a555e46256df5c529074399f76
Marcus Noble <marcus@marcusnoble.co.uk>
2015-08-13 19:37:00 +0100

    Added script to check for new other-things issues

---
+++ scripts/other-things.js
@@ -0,0 +1,36 @@
+// Description:
+//   Checks for "other-things" issues and lets the chatroom know.
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   None
+
+module.exports = function(robot){
+	var interval;
+  var room = "jsoxford/jsoxford.github.com";
+	var issuesURL = "https://api.github.com/repos/jsoxford/jsoxford.github.com/issues?labels=other-things";
+
+  robot.brain.set("lastcheck",new Date());
+
+	interval = setInterval(function(){
+		robot.http(issuesURL).get()(function(err, res, body){
+	      if(err) console.log(err);
+	      result = JSON.parse(body);
+	      if(result){
+	        result.forEach(function(issue){
+            var created = new Date(issue.created_at);
+            if(created > robot.brain.get("lastcheck")){
+              var message = "Check out this awesome thing #" + issue.number + " (" + issue.title + ")";
+              robot.messageRoom(room, message);
+            }
+          });
+	      }
+        robot.brain.set("lastcheck",new Date());
+	    });
+	}, 300000);
+}
