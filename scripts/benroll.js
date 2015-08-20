5c3d112d64e6da21212fc33a5ed86ff1023c435a
Daniel Pope <danielthepope@users.noreply.github.com>
2015-08-20 19:52:37 +0100

    Triggers from "help ben", "it is raining" and "oh yeah"
    I heard on Gitter earlier "We need your help Ben" and "It is raining", but JSOxBot failed to respond :(
    I considered allowing "Its raining" (without the apostrophe) but I decided poor grammar doesn't deserve an enlightening response.
    I thought I'd take off the exclamation mark from "oh yeah" while I was at it too.
    Also I'm okay with tabs and I'm okay with spaces, but I'm not okay with both being used at the same time for indentation ;)

--- scripts/benroll.js
+++ scripts/benroll.js
@@ -1,16 +1,16 @@
 // Benroll
 
 module.exports = function(robot) {
-  robot.hear(/help (me|us) (ben|@benfoxall)/i, function(msg){
+  robot.hear(/help (me |us |)(ben|@benfoxall)/i, function(msg){
     msg.send("https://twitter.com/omgmog/status/608556664334233600");
   });
   robot.hear(/(palace)/i, function(msg){
-  	msg.send("[This is relevant to the conversation](http://www.bensmindpalace.co.uk/)");
+    msg.send("[This is relevant to the conversation](http://www.bensmindpalace.co.uk/)");
   });
-  robot.hear(/it's raining/i, function(msg){
-  	msg.send("It's raining and @benfoxall is still wearing shorts.");
+  robot.hear(/it('| i)s raining/i, function(msg){
+    msg.send("It's raining and @benfoxall is still wearing shorts.");
   });
-  robot.hear(/Oh yeah!/i, function(msg){
-  	msg.send("![](https://files.gitter.im/jsoxford/jsoxford.github.com/JSbn/Ben.gif)");
+  robot.hear(/Oh yeah/i, function(msg){
+    msg.send("![](https://files.gitter.im/jsoxford/jsoxford.github.com/JSbn/Ben.gif)");
   });
 };
