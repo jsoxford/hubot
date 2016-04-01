4a3913e8427ee06a8c0c4c294c6fb6a29a3a2df7
Marcus Noble <marcus@marcusnoble.co.uk>
2016-04-01 18:31:20 +0100

    Send DM instead of flooding channel

--- scripts/code-of-conduct.js
+++ scripts/code-of-conduct.js
@@ -2,9 +2,7 @@ module.exports = function(robot) {
   var room = "test";
 
   robot.enter(function(msg) {
-    console.log(msg.message.user.name + ' joined ' + msg.message.room);
     if(msg.message.room === room) {
-        msg.send("Hi @" + msg.message.user.name + "! :wave: Please make sure you have read our Code of Conduct here: http://peterjwest.github.io/do-coc/");
         robot.send({room: msg.message.user.name}, "Hi @" + msg.message.user.name + "! :wave: Please make sure you have read our Code of Conduct here: http://peterjwest.github.io/do-coc/");
     }
   });
