724b5de98982d6fe9b5b459a852443464af407bd
Marcus Noble <marcus@marcusnoble.co.uk>
2016-04-08 20:12:12 +0100

    Added Doxford

--- scripts/meetup.js
+++ scripts/meetup.js
@@ -11,13 +11,14 @@
 //   when is the next event ?  - returns the next upcoming meetup event
 
 module.exports = function(robot) {
-  var multipleEvents = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_id=17778422%2C12345482%2C18829161%2C18789928%2C18617250&only=time%2Cevent_url%2Cname%2Cdescription%2Cyes_rsvp_count%2Crsvp_limit&photo-host=secure&page=20&fields=&order=time&status=upcoming&desc=false&sig_id=153356042&sig=f5a37aa0b09d2a1a48f8e82d5e194fd2494a409c";
+  var multipleEvents = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_id=17778422%2C19558444%2C12345482%2C18829161%2C18789928%2C18617250&only=time%2Cevent_url%2Cname%2Cdescription%2Cyes_rsvp_count%2Crsvp_limit&photo-host=secure&page=20&fields=&order=time&status=upcoming&desc=false&sig_id=153356042&sig=44da064bc2450a56f4694fd390c69da406cee00f";
   var jsOxfordEvents = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_id=17778422&only=time%2Cevent_url%2Cname%2Cdescription%2Cyes_rsvp_count%2Crsvp_limit&photo-host=secure&page=20&fields=&order=time&desc=false&status=upcoming&sig_id=153356042&sig=84e9ac6ce37bdb3c00e4f82fe5a7ce798865fbe4";
   var uxOxfordEvents = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=UX-Oxford&only=time%2Cevent_url%2Cname%2Cdescription%2Cyes_rsvp_count%2Crsvp_limit&photo-host=secure&page=20&fields=&order=time&status=upcoming&desc=false&sig_id=153356042&sig=04a4edfbcbab17f591a8ad208da4b02bf364e24c";
   var phpOxfordEvents = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=PHP-Oxford&only=time%2Cevent_url%2Cname%2Cdescription%2Cyes_rsvp_count%2Crsvp_limit&photo-host=public&page=20&fields=&order=time&status=upcoming&desc=false&sig_id=153356042&sig=2893753701aa73225565fc154a447ced1d0b20bb";
   var dockerEvents = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=docker-oxford&only=time%2Cevent_url%2Cname%2Cdescription%2Cyes_rsvp_count%2Crsvp_limit&photo-host=public&page=20&fields=&order=time&status=upcoming&desc=false&sig_id=153356042&sig=691ed0980f6543572bb425f7d1675e911e592553";
   var rubyEvents = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=Oxford-Ruby-Users-Group-OxRUG&only=time%2Cevent_url%2Cname%2Cdescription%2Cyes_rsvp_count%2Crsvp_limit&photo-host=public&page=20&fields=&order=time&status=upcoming&desc=false&sig_id=153356042&sig=28283c85246a091d9cc0dff99aac1c1bbd2941a5";
   var pythonEvents = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=oxfordpython&only=time%2Cevent_url%2Cname%2Cdescription%2Cyes_rsvp_count%2Crsvp_limit&photo-host=secure&page=20&fields=&order=time&status=upcoming&desc=false&sig_id=153356042&sig=f2b06efd17bf9cafe4a6e82f35ea0a28639d06f2";
+  var doxford = "https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=doxford&only=time%2Cevent_url%2Cname%2Cdescription%2Cyes_rsvp_count%2Crsvp_limit&photo-host=secure&page=20&fields=&order=time&status=upcoming&desc=false&sig_id=153356042&sig=bcf08dbdc0a70403f873540f11c2dbeb60ea1fc7";
   var result;
 
   robot.hear(/when(s|'s| is) the next (.*)(meetup|event)\s?\?/i, function(msg){
@@ -28,7 +29,7 @@ module.exports = function(robot) {
     if(!community) {
       if(room.toLowerCase().indexOf('jsox') >= 0) {
         meetupURL = jsOxfordEvents;
-      } else if(room.toLowerCase().indexOf('ux') >= 0) {
+      } else if(room.toLowerCase().indexOf('uxui') >= 0) {
         meetupURL = uxOxfordEvents;
       } else if(room.toLowerCase().indexOf('php') >= 0) {
         meetupURL = phpOxfordEvents;
@@ -38,6 +39,8 @@ module.exports = function(robot) {
         meetupURL = rubyEvents;
       } else if(room.toLowerCase().indexOf('python') >= 0) {
         meetupURL = pythonEvents;
+      } else if(room.toLowerCase().indexOf('devop') >= 0) {
+        meetupURL = doxford;
       }
     } else {
       if(community.toLowerCase().indexOf('jsox') >= 0) {
@@ -52,6 +55,8 @@ module.exports = function(robot) {
         meetupURL = rubyEvents;
       } else if(community.toLowerCase().indexOf('python') >= 0) {
         meetupURL = pythonEvents;
+      } else if(community.toLowerCase().indexOf('doxford') >= 0) {
+        meetupURL = doxford;
       }
     }
 
