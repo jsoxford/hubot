12dc1dd51623024dcb05479de289c6bfdde0295a
Marcus Noble <AverageMarcus@users.noreply.github.com>
2016-11-14 15:30:48 +0000

    Pretty sure this fixes #54 but haven't had a chance to test

--- scripts/meetup.js
+++ scripts/meetup.js
@@ -64,8 +64,8 @@ module.exports = function (robot) {
     });
   }
 
-  robot.hear(/^(?:when|what)(?:s|'s| is) the next (.*)(?:meetup|event|talk|party|hack|shindig|gathering|meeting|happening)/i, processMessage);
-  robot.respond(/(?:when|what)(?:s|'s| is) the next (.*)(?:meetup|event|talk|party|hack|shindig|gathering|meeting|happening)/i, processMessage);
+  robot.hear(/^(?:when|what)(?:.?s| is) the next (.*)(?:meetup|event|talk|party|hack|shindig|gathering|meeting|happening)/i, processMessage);
+  robot.respond(/(?:when|what)(?:.?s| is) the next (.*)(?:meetup|event|talk|party|hack|shindig|gathering|meeting|happening)/i, processMessage);
   robot.hear(/meetup\.com\/[^\/]+\/events\/(\d+)/i, meetupInfo);
 }
 
